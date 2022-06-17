fab9670a1dfdfb8696cf5432b9c9dc6c94e95e6b
gaohanyu <2405662372@qq.com>
2022-06-17 22:09:45 +0800

    Rest映射及源码解析和改变默认的_method

---
+++ src/main/java/com/ghy/boot/controller/ResController.java
@@ -0,0 +1,27 @@
+package com.ghy.boot.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ResController {
+    //@RequestMapping(value = "/user",method = RequestMethod.GET)
+    @GetMapping("/user")
+    public String getUser(){
+        return "GET-张三";
+    }
+    //RequestMapping(value = "/user",method = RequestMethod.POST)
+    @PostMapping("/user")
+    public String saveUser(){
+        return "POST-张三";
+    }
+    //@RequestMapping(value = "/user",method = RequestMethod.PUT)
+    @PutMapping("/user")
+    public String putUser(){
+        return "PUT-张三";
+    }
+    //@RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    @DeleteMapping("/user")
+    public String deleteUser(){
+        return "DELETE-张三";
+    }
+}
