75b66a10ef2b136360c54f5fc8f3e291ae3fe0d8
gaohanyu <2405662372@qq.com>
2022-06-19 20:23:32 +0800

    浏览器与postman内容协商完全适配

--- src/main/java/com/ghy/boot/config/WebConfig.java
+++ src/main/java/com/ghy/boot/config/WebConfig.java
@@ -1,15 +1,26 @@
 package com.ghy.boot.config;
 
+import com.ghy.boot.GuiguMessageConverter;
 import com.ghy.boot.bean.Pet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.StringUtils;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.accept.ParameterContentNegotiationStrategy;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration(proxyBeanMethods = false)
 public class WebConfig /*implements WebMvcConfigurer*/{
@@ -28,9 +39,33 @@ public class WebConfig /*implements WebMvcConfigurer*/{
         configurer.setUrlPathHelper(urlPathHelper);
     }*/
 
-    @Bean   //注入WebMvcConfigurer组件
+    //注入WebMvcConfigurer组件
+    @Bean
     public WebMvcConfigurer webMvcConfigurer(){
         return new WebMvcConfigurer() {
+            //自定义内容协商策略
+            @Override
+            public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+                //Map<String,MediaType> mediaTypes
+                Map<String, MediaType> mediaTypes = new HashMap<>();
+                //指定支持解析哪些参数对应的哪些媒体类型
+                mediaTypes.put("json",MediaType.APPLICATION_JSON);
+                mediaTypes.put("xml",MediaType.APPLICATION_XML);
+                mediaTypes.put("gg",MediaType.parseMediaType("application/x-guigu"));
+                // 基于参数的
+                ParameterContentNegotiationStrategy parameterStrategy = new ParameterContentNegotiationStrategy(mediaTypes);
+                //parameterStrategy.setParameterName("ff");
+                // 基于请求头的
+                HeaderContentNegotiationStrategy headerStrategy = new HeaderContentNegotiationStrategy();
+                configurer.strategies(Arrays.asList(parameterStrategy,headerStrategy));
+            }
+
+            //自定义converter
+            @Override
+            public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+                converters.add(new GuiguMessageConverter());
+            }
+
             @Override
             public void configurePathMatch(PathMatchConfigurer configurer) {
                 UrlPathHelper urlPathHelper = new UrlPathHelper();
--- src/main/java/com/ghy/boot/controller/ResponseTestController.java
+++ src/main/java/com/ghy/boot/controller/ResponseTestController.java
@@ -8,7 +8,18 @@ import java.util.Date;
 
 @Controller
 public class ResponseTestController {
-    @ResponseBody   //给前端自动返回json数据
+    /*
+    * 1 浏览器发请求直接返回 xml       [applicatiom/xml]   jacksonXmlConverter
+    * 2 如果是ajax请求 返回 json      [application/json]   jacksonJsonConverter
+    * 3 如果硅谷app发请求，返回自定义协议数据    [application/x-guigu]   xxxConverter
+    *       要求： 属性值1;属性值2;
+    *
+    * 步骤：
+    *   1 添加自定义的MessageConverter进系统底层
+    *   2 系统底层每次内容协商时就会统计出所有MessageConverter能操作那些类型
+    *   3 客户端内容协商[x-guigu ---> guigu]
+    * */
+    @ResponseBody
     @GetMapping("/test/person")
     public Person person(){
         Person p = new Person();
